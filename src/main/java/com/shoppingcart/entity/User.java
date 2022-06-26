8900a6afcd7e1457c428738a66d7a31243cf9a30
prashant.kumar2 <prashant.kumar2@thoughtworks.com>
2022-06-26 11:16:59 +0530

    Added code for Creation of user table

---
+++ src/main/java/com/shoppingcart/entity/User.java
@@ -0,0 +1,16 @@
+package com.shoppingcart.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String name;
+    private String address;
+}
